6325b14c8b2986a5f014a2212413181cea7e31df
Bahulvgopal <145193440+Bahulvgopal@users.noreply.github.com>
2026-09-19 19:16:40 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      };
-    }
-    return config;
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
+
+  async redirects() {
+    return [
+      {
+        source: "/intern",
+        destination: "https://forms.gle/QkhLhpXYreS2omjL7",
+        permanent: false,
+      },
+    ];
   },
 };
 
